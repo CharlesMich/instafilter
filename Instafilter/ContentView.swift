cb14bd42510cb7bfe3b3441c56c5376bd030fba2
CharlesMich <charleedongo@hotmail.com>
2024-11-14 15:29:13 -0600

    first commit

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+   
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .blur(radius: blurAmount)
+            Slider(value: $blurAmount)
+                .onChange(of: blurAmount){oldValue, newValue in
+                    print("New value is \(newValue)")
+                    
+                }
+            Button("Random Blur"){
+                blurAmount = Double.random(in: 0...20)
+            }
         }
         .padding()
     }
