ece0b7e57bb20c1fe4804a9a42e4a711c9642343
CharlesMich <charleedongo@hotmail.com>
2024-11-14 14:37:52 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Charles Michael on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
